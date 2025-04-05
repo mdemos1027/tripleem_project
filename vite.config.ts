23e3073df634d83872355ad43d584c643c746d5f
mdemos1027 <minas.d@mmmfintech.io>
2025-04-05 18:12:48 +0300

    fix build in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  build: {
+    outDir: 'build', // Change this to 'build' instead of 'dist'
+  }
 })
