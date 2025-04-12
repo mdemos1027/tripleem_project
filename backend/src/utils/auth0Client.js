2ad6e78eff6ab77e369b269be0e077bcfa78c10f
mdemos1027 <minas.d@mmmfintech.io>
2025-04-12 10:01:23 +0300

    added auth0client file

---
+++ backend/src/utils/auth0Client.js
@@ -0,0 +1,8 @@
+import { ManagementClient } from 'auth0';
+
+export const auth0 = new ManagementClient({
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  scope: 'create:users'
+});
